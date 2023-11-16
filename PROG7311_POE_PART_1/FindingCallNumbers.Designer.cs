7f5e603bbb2217a5faf19bf1af311c68577d2163
EwanJames69 <ewanjames52@gmail.com>
2023-11-16 20:31:25 +0200

    Did not commit everything from the last commit so I gotta do it again lol, my bad G.

--- PROG7311_POE_PART_1/FindingCallNumbers.Designer.cs
+++ PROG7311_POE_PART_1/FindingCallNumbers.Designer.cs
@@ -28,56 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTest = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.lblCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblTest
-            // 
-            this.lblTest.Location = new System.Drawing.Point(12, 9);
-            this.lblTest.Name = "lblTest";
-            this.lblTest.Size = new System.Drawing.Size(805, 351);
-            this.lblTest.TabIndex = 0;
-            this.lblTest.Text = "label1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(-1, 718);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // lblCount
-            // 
-            this.lblCount.AutoSize = true;
-            this.lblCount.Location = new System.Drawing.Point(183, 452);
-            this.lblCount.Name = "lblCount";
-            this.lblCount.Size = new System.Drawing.Size(44, 16);
-            this.lblCount.TabIndex = 2;
-            this.lblCount.Text = "label1";
             // 
             // FindingCallNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(829, 743);
-            this.Controls.Add(this.lblCount);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblTest);
+            this.ClientSize = new System.Drawing.Size(977, 645);
             this.Name = "FindingCallNumbers";
             this.Text = "FindingCallNumbers";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblTest;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label lblCount;
     }
 }
